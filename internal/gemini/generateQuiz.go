043a8e7bd3874138b507ac2b29118542120b715c
Ethan Lee <ethantlee21@gmail.com>
2025-09-27 06:13:10 -0400

    prompt engineering

--- internal/gemini/generateQuiz.go
+++ internal/gemini/generateQuiz.go
@@ -43,9 +43,7 @@ func GenerateQuizWorker(cfg *config.Config, ctx context.Context, wg *sync.WaitGr
 			root := filepath.Join(cfg.SchoolPath, code)
 
 			var files []string
-
-			slog.Info("trying to walk dir", slog.String("path", root))
-
+			
 			err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
 				if err != nil {
 					slog.Error("Error retrieving file paths.")
@@ -58,7 +56,6 @@ func GenerateQuizWorker(cfg *config.Config, ctx context.Context, wg *sync.WaitGr
 			if err != nil {
 				errCh <- fmt.Errorf("failed to retrieve file paths: %w", err)
 			}
-			slog.Info("Walk through dir", slog.String("path", root))
 
 			slog.Info("Start generating quiz")
 			msg, err := prepMessage(generateQuestionPrompt, files...)
@@ -67,16 +64,11 @@ func GenerateQuizWorker(cfg *config.Config, ctx context.Context, wg *sync.WaitGr
 				continue
 			}
 
-			slog.Info("Finished preparing messages")
-
 			res, err := sendMessage(cfg, ctx, msg, genai_config)
 			if err != nil {
 				errCh <- fmt.Errorf("failed to send message to Gemini: %w", err)
 				continue
 			}
-			slog.Info("Finished generating quiz")
-
-			slog.Info(res)
 
 			qaPairs := []model.Question{}
 			if err := json.Unmarshal([]byte(res), &qaPairs); err != nil {
@@ -106,19 +98,14 @@ func GenerateQuizWorker(cfg *config.Config, ctx context.Context, wg *sync.WaitGr
 const generateQuestionPrompt = `
 You are given a set of input documents about a college course. 
 Your task is to carefully read and understand the content, 
-then generate 10 multiple choice questions with 4 corresponding answer choices with one correct answer.
-The question should capture the most important and
-relevant concepts from the documents. 
-Store each question and corresponding answer strictly in a json object, 
-and return only an array of the 10 top-level json object with valid fields:
-Return an array of json objects wrapped in [...]
+then generate 10 multiple choice questions with 4 answer choices with one correct answer.
+The question should capture the most important and relevant concepts from the documents. 
+The response schema is as follows:
 - "question":
-	- Must be a string 
 	- Try to ask about concepts that can be confusing to students
 - "choices":
-	- Must be an array of string of length 4.
+	- Choices the user should choose from
 - "answer:
-	- Must be type int
-	- The correct answer value should match with the correct choices
-ex: [{"question":"Calculate the triangle's area with sides 4 and 5cm.","choices":["3", "6","8", "10"],"answer":3}, {"question":"When is Abraham Lincoln's birthday?","choices":["February 12th, 1809","October 3rd, 2004", "January 1st, 2006", "August 28th, 1838"], "answer":"February 12th, 1809", "answer":0} ] 
+	- The correct answer value should be the array index of correct choices
+	- Try to randomize the correct answer index, so questions don't have a pattern of answers
 `
